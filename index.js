289ffed02c6dbecd12fd9aa511ef2e05cd94946b
Ganesh Annan <gannan@digitalbazaar.com>
2018-06-05 16:41:06 -0400

    Move service.

--- index.js
+++ index.js
@@ -5,13 +5,12 @@
 
 import {store as defaultStore} from 'bedrock-web-store';
 import Session from './Session.js';
-import SessionService from './SessionService.js';
 
 export const getSession = async (
   {id = 'session.default', store = defaultStore} = {}) => {
   let session = await store.get({id});
   if(session === undefined) {
-    session = new Session({service: SessionService});
+    session = new Session();
     await store.create({id, object: session});
     await session.refresh();
   }
