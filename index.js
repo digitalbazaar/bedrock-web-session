d2c288164be5a6d7da18e2cfbdb124546890e866
Dave Longley <dlongley@digitalbazaar.com>
2018-06-06 11:02:23 -0400

    Add `DuplicateError` check.

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ import Session from './Session.js';
 
 export const getSession = async (
   {id = 'session.default', store = defaultStore} = {}) => {
-  let session = await store.get({id});
-  if(session === undefined) {
-    session = new Session();
-    await store.create({id, object: session});
-    await session.refresh();
+  try {
+    const session = new Session();
+    return await store.create({id, object: session});
+  } catch(e) {
+    if(e.name === 'DuplicateError') {
+      return store.get({id});
+    }
+    throw e;
   }
-  return session;
 };
