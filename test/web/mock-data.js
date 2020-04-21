fa11b0837c9f5f1d4b4199394b42798026ffad75
Matthew Collier <mcollier@digitalbazaar.com>
2020-04-21 14:49:28 -0400

    Setup CI and coverage workflow.

---
+++ test/web/mock-data.js
@@ -0,0 +1,9 @@
+/*!
+ * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+// const {constants} = require('security-context');
+
+const mocks = {};
+module.exports = mocks;
