c53548fff503ed92eef2cb9bbc281e0d1c5c202e
andrewjones <ajones@digitalbazaar.com>
2020-07-21 09:31:35 -0400

    Add tests for create and refresh session.

--- test/web/10-profile.js
+++ test/web/10-profile.js
@@ -2,7 +2,8 @@
  * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
  */
 
-import {SessionService} from 'bedrock-web-session';
+import {SessionService, createSession} from 'bedrock-web-session';
+import {MemoryEngine} from 'bedrock-web-store';
 
 const sessionService = new SessionService();
 // import mockData from './mock-data.js';
@@ -11,8 +12,7 @@ describe('session API', function() {
   describe('get API', function() {
     describe('unauthenticated request', function() {
       it('should have a session with out an account', async function() {
-        let result;
-        let err;
+        let result, err = null;
         try {
           result = await sessionService.get();
         } catch(e) {
@@ -20,6 +20,29 @@ describe('session API', function() {
         }
         should.not.exist(err);
         should.exist(result);
+      });
+      it('should create a session', async function() {
+        let session, err = null;
+        try {
+          session = await createSession(
+            {id: 'unauthenticated-create-test', store: new MemoryEngine()});
+        } catch(e) {
+          err = e;
+        }
+        should.exist(session);
+        should.not.exist(err);
+      });
+      it('should refresh a session', async function() {
+        let session, err = null;
+        try {
+          session = await createSession(
+            {id: 'unauthenticated-refresh-test', store: new MemoryEngine()});
+          await session.refresh();
+        } catch(e) {
+          err = e;
+        }
+        should.exist(session);
+        should.not.exist(err);
       });
     });
     describe.skip('authenticated request', function() {
