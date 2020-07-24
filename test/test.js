da959b3ea96091a931164da4c7f0897a0f4f5f2d
Andrew L Jones <ajones@digitalbazaar.com>
2020-07-24 16:57:12 -0400

    Add deps.

--- test/test.js
+++ test/test.js
@@ -3,18 +3,16 @@
  */
 const bedrock = require('bedrock');
 require('bedrock-mongodb');
+require('bedrock-express');
+// note: this is the only backend we currently
+// have for sessions.
+require('bedrock-session-mongodb');
 require('bedrock-https-agent');
 require('bedrock-security-context');
-
-const brPassport = require('bedrock-passport');
-const mockData = require('./web/mock-data');
-brPassport.optionallyAuthenticated = (req, res, next) => {
-  req.user = {
-    account: {},
-    actor: mockData.actors.alpha
-  };
-  next();
-};
+require('bedrock-authn-token');
+require('bedrock-authn-token-http');
+require('bedrock-account');
+require('bedrock-account-http');
 
 require('bedrock-test');
 require('bedrock-karma');
