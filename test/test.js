9f53e58add5f4e7009215cc8a4f86de32776ee35
Andrew L Jones <ajones@digitalbazaar.com>
2020-08-03 11:48:58 -0400

    Add an unauthenticated test.

--- test/test.js
+++ test/test.js
@@ -4,16 +4,16 @@
 const bedrock = require('bedrock');
 require('bedrock-mongodb');
 require('bedrock-express');
+require('bedrock-session-http');
 // note: this is the only backend we currently
 // have for sessions.
 require('bedrock-session-mongodb');
-require('bedrock-https-agent');
-require('bedrock-security-context');
 require('bedrock-authn-token');
 require('bedrock-authn-token-http');
 require('bedrock-account');
 require('bedrock-account-http');
-
+require('bedrock-https-agent');
+require('bedrock-security-context');
 require('bedrock-test');
 require('bedrock-karma');
 
