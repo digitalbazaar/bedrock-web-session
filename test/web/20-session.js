02d7fd33da7c55ae6345e5b8b6b5c37dc7db9253
Dave Longley <dlongley@digitalbazaar.com>
2022-01-22 17:51:02 -0500

    Improve test coverage.

--- test/web/20-session.js
+++ test/web/20-session.js
@@ -32,6 +32,28 @@ describe('session API', () => {
       // an unauthenticated session has no data
       session.data.should.eql({});
     });
+    it('should detect double creation of the session singleton', async () => {
+      let err;
+      let s;
+      try {
+        s = await createSession();
+      } catch(e) {
+        err = e;
+      }
+      should.not.exist(err);
+      should.exist(s);
+      should.exist(session);
+
+      s = null;
+      try {
+        s = await createSession();
+      } catch(e) {
+        err = e;
+      }
+      should.exist(err);
+      should.not.exist(s);
+      err.message.should.equal('Session singleton already created.');
+    });
   }); // end unauthenticated request
 
   describe('authenticated request', () => {
@@ -122,7 +144,7 @@ describe('session API', () => {
       session.data.account.should.be.an('object');
       session.data.account.should.have.property('id');
       session.data.account.id.should.equal(account.id);
-      let changed = false;
+      let changed = 0;
       const remove = session.on(
         'change', async ({oldEnded, oldData, newData}) => {
           should.exist(oldEnded);
@@ -134,16 +156,58 @@ describe('session API', () => {
           // need to be used to determine what behavior a `change` listener
           // will take
           oldEnded.should.eql(false);
-          changed = true;
-          remove();
+          changed++;
         });
       await delay(2000);
       await session.refresh();
       session.should.have.keys(['data', '_service', '_eventTypeListeners']);
       // an unauthenticated session has no data
       session.data.should.eql({});
-      // change listener should have been called
-      changed.should.eql(true);
+      // change listener should have been called once
+      changed.should.eql(1);
+      remove();
+    });
+    it('should emit change event twice and end session on error', async () => {
+      should.exist(session);
+      session.should.be.an('object');
+      session.should.have.keys(['data', '_service', '_eventTypeListeners']);
+      session.data.should.be.an('object');
+      session.data.should.have.keys(['account']);
+      session.data.account.should.be.an('object');
+      session.data.account.should.have.property('id');
+      session.data.account.id.should.equal(account.id);
+      let changed = 0;
+      const remove = session.on(
+        'change', async ({oldEnded, oldData, newData}) => {
+          changed++;
+          should.exist(oldEnded);
+          should.exist(oldData);
+          should.exist(newData);
+          if(changed === 1) {
+            oldData.should.not.eql(newData);
+            oldEnded.should.eql(false);
+            throw new Error('change listener error');
+          } else {
+            // should be ending on the second call due to the error from the
+            // first
+            oldEnded.should.eql(true);
+          }
+        });
+      await delay(2000);
+      let err;
+      try {
+        await session.refresh();
+      } catch(e) {
+        err = e;
+      }
+      should.exist(err);
+      err.message.should.eql('change listener error');
+      session.should.have.keys(['data', '_service', '_eventTypeListeners']);
+      // an unauthenticated session has no data
+      session.data.should.eql({});
+      // change listener should have been called twice
+      changed.should.eql(2);
+      remove();
     });
     it('should emit change event on session.end', async function() {
       should.exist(session);
@@ -155,7 +219,7 @@ describe('session API', () => {
       session.data.account.should.be.an('object');
       session.data.account.should.have.property('id');
       session.data.account.id.should.equal(account.id);
-      let changed = false;
+      let changed = 0;
       const remove = session.on(
         'change', async ({oldEnded, oldData, newData}) => {
           should.exist(oldEnded);
@@ -163,15 +227,52 @@ describe('session API', () => {
           should.exist(newData);
           oldData.should.not.eql(newData);
           oldEnded.should.eql(true);
-          changed = true;
-          remove();
+          changed++;
         });
       await session.end();
       session.should.have.keys(['data', '_service', '_eventTypeListeners']);
       // data should now be empty
       session.data.should.eql({});
-      // change listener should have been called
-      changed.should.eql(true);
+      // change listener should have been called once
+      changed.should.eql(1);
+      remove();
+    });
+    it('should emit change event once on session.end ' +
+      'when change listener throws error', async function() {
+      should.exist(session);
+      session.should.be.an('object');
+      session.should.have.keys(['data', '_service', '_eventTypeListeners']);
+      session.data.should.be.an('object');
+      // an authenticated session has data
+      session.data.should.have.keys(['account']);
+      session.data.account.should.be.an('object');
+      session.data.account.should.have.property('id');
+      session.data.account.id.should.equal(account.id);
+      let changed = 0;
+      const remove = session.on(
+        'change', async ({oldEnded, oldData, newData}) => {
+          should.exist(oldEnded);
+          should.exist(oldData);
+          should.exist(newData);
+          oldData.should.not.eql(newData);
+          oldEnded.should.eql(true);
+          changed++;
+          throw new Error('change listener error');
+        });
+      let err;
+      try {
+        await session.end();
+      } catch(e) {
+        err = e;
+      }
+      should.exist(err);
+      err.message.should.eql('change listener error');
+      session.should.have.keys(['data', '_service', '_eventTypeListeners']);
+      // data should now be empty
+      session.data.should.eql({});
+      // change listener should have been called once
+      changed.should.eql(1);
+      remove();
     });
   }); // end authenticated request
 });
